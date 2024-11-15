df47ab2a083d0a03aa74c72ec5af2f0f28b51984
John Kennedy <johnkmedeiros@outlook.com>
2024-11-15 13:29:42 -0300

    feat(movies) movies and favorites migrations

---
+++ database/migrations/2024_11_15_134802_create_table_movies.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('themoviedb_id')->index();
+            $table->string('title');
+            $table->text('overview')->nullable();
+            $table->date('release_date');
+            $table->string('poster_path')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
